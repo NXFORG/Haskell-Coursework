85edbed3d22678a8114443479690c3d66a377d54
Chris <christopher.anstead@yahoo.co.uk>
2019-04-08 16:06:03 +0100

    Completed between 2 years function and searching for prefix function

--- FPUP854443.hs
+++ FPUP854443.hs
@@ -45,10 +45,25 @@ albumsToString :: [Album] -> String
 albumsToString [] = []
 albumsToString (x:xs) = title x ++ " " ++ artist x ++ " " ++ show (year x) ++ " " ++ show (sales x) ++ "\n" ++ albumsToString xs
 
+
 top10 :: [Album] -> [Album]
 top10 (x:xs) = take 10 (x:xs)
 
+getBetween :: [Album] -> [Album]
+getBetween (x:xs)
+      | year x < 2000 || year x > 2008 = getBetween xs
+      | year x >= 2000 && year x <= 2008 = [x] ++ getBetween xs
 
+
+getPrefix :: [Album] -> [Album]
+getPrefix (x:xs)
+      | isPrefixOf "Th" (title x) == False = getPrefix xs
+      | isPrefixOf "Th" (title x) == True = [x] ++ getPrefix xs
+
+--getSales :: [Album] -> String
+--getSales (x:xs)
+    --  | artist x /= "Queen" = getSales xs
+    --  | artist x == "Queen" = sales x + getSales xs
 
 -- Demo function to test basic functionality (without persistence - i.e.
 -- testData doesn't change and nothing is saved/loaded to/from albums file).
@@ -56,8 +71,8 @@ top10 (x:xs) = take 10 (x:xs)
 demo :: Int -> IO ()
 demo 1  = putStrLn (albumsToString testData)
 demo 2  = putStrLn (albumsToString (top10 testData))
---demo 3  = putStrLn ( all albums released between 2000 and 2008 inclusive )
---demo 4  = putStrLn ( all albums with titles beginning with "Th" )
+demo 3  = putStrLn (albumsToString (getBetween testData))
+demo 4  = putStrLn (albumsToString (getPrefix testData))
 --demo 5  = putStrLn ( total sales figure for "Queen" )
 --demo 6  = putStrLn ( all artists with the number of times they appear in top 50 )
 --demo 7  = putStrLn ( albums after removing 50th album and adding "Progress"
